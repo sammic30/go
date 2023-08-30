01c854f0fc5e03f64b6f0f8465f7bbf2bb0db314
sammic30 <sammi.c30@gmail.com>
2023-08-30 15:03:05 -0700

    Create helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+//  func main() is a entry point of the go program, we don't need to explicitly call it.
+//  fmt.Println("Hello, World!") - fmt is a package and Println is a function
